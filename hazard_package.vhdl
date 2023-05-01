72d07d58e6cbc1b8b48e66a17921d68dc90ad905
VR-1103 <vedika111103@gmail.com>
2023-05-01 16:01:30 +0530

    hazard_rr updated

--- hazard_package.vhdl
+++ hazard_package.vhdl
@@ -12,12 +12,21 @@ end entity;
 architecture hazardous of hazard_RR is
 	signal LHI_LW, rrex, exma, mawb, rrex_checked, exma_checked, mawb_checked: std_logic := '0';
 begin
-rrex <= '1' when((RREX_opcode = "0011" and IDRR_11_9 = RREX_11_9) or (RREX_opcode = "0100" and IDRR_11_9 = RREX_11_9)) else '0';
+rrex <= '1' when((RREX_opcode = "0011" and IDRR_11_9 = RREX_11_9) or (RREX_opcode = "0100" and IDRR_11_9 = RREX_11_9) or 
+						(RREX_opcode = "0011" and IDRR_8_6 = RREX_11_9) or (RREX_opcode = "0100" and IDRR_8_6 = RREX_11_9)) else '0';
+
 rrex_checked <= '1' when (rrex = '1' and RREX_11_9 != "000") else '0';
-exma <= '1' when((EXMA_opcode = "0011" and IDRR_11_9 = EXMA_11_9) or (EXMA_opcode = "0100" and IDRR_11_9 = EXMA_11_9)) else '0';
+
+exma <= '1' when((EXMA_opcode = "0011" and IDRR_11_9 = EXMA_11_9) or (EXMA_opcode = "0100" and IDRR_11_9 = EXMA_11_9) or 
+						(EXMA_opcode = "0011" and IDRR_8_6 = EXMA_11_9) or (EXMA_opcode = "0100" and IDRR_8_6 = EXMA_11_9)) else '0';
+
 exma_checked <= '1' when (exma = '1' and EXMA_11_9 != "000") else '0';
-mawb <= '1' when((MAWB_opcode = "0011" and IDRR_11_9 = MAWB_11_9) or (MAWB_opcode = "0100" and IDRR_11_9 = MAWB_11_9)) else '0';
+
+mawb <= '1' when((MAWB_opcode = "0011" and IDRR_11_9 = MAWB_11_9) or (MAWB_opcode = "0100" and IDRR_11_9 = MAWB_11_9) or 
+						(MAWB_opcode = "0011" and IDRR_8_6 = MAWB_11_9) or (MAWB_opcode = "0100" and IDRR_8_6 = MAWB_11_9)) else '0';
+
 mawb_checked <= '1' when (mawb = '1' and MAWB_11_9 != "000") else '0';
+
 LHI_LW <= '1' when(rrex_checked = '1' or exma_checked = '1' or mawb_checked = '1') else '0';
 stall_IFID <= LHI_LW;
 stall_IDRR <= LHI_LW;
